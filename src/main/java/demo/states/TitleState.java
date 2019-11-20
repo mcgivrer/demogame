df4bfc199ee2bd1d21e6778ca80a9475c31dc397
Frédéric Delorme <frederic.delorme@gmail.com>
2019-11-20 02:41:09 +0100

    feat(TitleState): Add a Title State
    - Add a TitleState extending AbstractState with title and some basic info
      (license, authors).
    - Add a new TextObject to manage some dynamic text.
    
    fix #28

---
+++ src/main/java/demo/states/TitleState.java
@@ -0,0 +1,50 @@
+package demo.states;
+
+import core.Game;
+import core.Renderer;
+import core.state.AbstractState;
+import core.state.State;
+
+import java.awt.*;
+
+public class TitleState extends AbstractState implements State {
+    @Override
+    public void input(Game g) {
+
+    }
+
+    @Override
+    public void initialize(Game g) {
+
+    }
+
+    @Override
+    public void load(Game g) {
+
+    }
+
+    @Override
+    public boolean isLoaded() {
+        return false;
+    }
+
+    @Override
+    public void update(Game g, float elapsed) {
+
+    }
+
+    @Override
+    public void render(Game g, Renderer r) {
+
+    }
+
+    @Override
+    public void dispose(Game g) {
+
+    }
+
+    @Override
+    public void drawHUD(Game ga, Renderer r, Graphics2D g) {
+
+    }
+}
