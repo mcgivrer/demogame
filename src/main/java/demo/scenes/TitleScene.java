2dc145e572be444b7a211a90e851c3e2babe215b
Frédéric Delorme <frederic.delorme@gmail.com>
2020-10-23 12:24:45 +0200

    feat(AddTitle): Add a TitleScene
    - update the game.json file to add title entry
    - add TitleScene in the demo package
    
    start #28

---
+++ src/main/java/demo/scenes/TitleScene.java
@@ -0,0 +1,52 @@
+package demo.scenes;
+
+import java.awt.Graphics2D;
+
+import core.Game;
+import core.gfx.Renderer;
+import core.scene.AbstractScene;
+
+class TitleScene extends AbstractScene {
+
+    @Override
+    public void load(Game g) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public boolean isLoaded() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void input(Game g) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void update(Game g, double elapsed) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void render(Game g, Renderer r, double elpased) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void dispose(Game g) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void drawHUD(Game ga, Renderer r, Graphics2D g) {
+        // nothing to do there.
+
+    }
+
+}
